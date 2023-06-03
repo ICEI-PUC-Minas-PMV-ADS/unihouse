85949a6ed7512066220e819a431c9773cf0d5bf9
Kayki Letieri <kaykiletieri37@gmail.com>
2023-06-03 15:45:06 -0300

    feat: add sistema de autenticacao

---
+++ src/views/js/login_script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("loginForm").addEventListener("submit", function(event) {
+      event.preventDefault();
+  
+      var username = document.getElementById("usernameInput").value;
+      var password = document.getElementById("passwordInput").value;
+  
+      if (!username || !password) {
+        alert("Por favor, preencha todos os campos.");
+        return false;
+      }
+  
+      var storedUsers = localStorage.getItem("registeredUsers");
+      var users = storedUsers ? JSON.parse(storedUsers) : [];
+  
+      var authenticatedUser = users.find(function(user) {
+        return user.username === username && user.password === password;
+      });
+  
+      if (authenticatedUser) {
+        alert("Login bem-sucedido!");
+  
+       
+        window.location.href = "../../index.html";
+      } else {
+        alert("Usuário ou senha inválidos. Por favor, tente novamente.");
+      }
+  
+      return false;
+    });
+  });
+  
