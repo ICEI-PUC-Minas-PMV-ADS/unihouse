f60c8277ed719fe3d9a6c784ec67a132acd05d9f
Kayki Letieri <kaykiletieri37@gmail.com>
2023-06-12 19:19:41 -0300

    feat: release 1.0.0

--- src/views/js/registerAccommodation_script.js
+++ src/views/js/registerAccommodation_script.js
@@ -1,4 +1,4 @@
-const form   = document.getElementById('form');
+const form   = document.getElementById('informacoes--primarias');
     const campos = document.querySelectorAll('.required');
     const spans  = document.querySelectorAll('.span-required');
     const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
@@ -40,3 +40,70 @@ const form   = document.getElementById('form');
             removeError(1);
         }
     }
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("informacoes--primarias").addEventListener("submit", function(event) {
+        event.preventDefault();
+    
+        var nomeALJ = document.getElementById("iptNameALJ").value;
+        var ruaALJ = document.getElementById("iptRuaALJ").value;
+        var cidadeALJ = document.getElementById("iptCidade").value;
+        var bairoALJ = document.getElementById("iptBairro").value;
+        var estadoALJ = document.getElementById("iptEstado").value;
+        var complementoALJ = document.getElementById("iptComplemento").value;
+        var cepALJ = document.getElementById("iptCep").value;
+        var escALJ = document.getElementsByName("iptEscCasa");
+        var nomeLOC = document.getElementById("iptNameloc").value;
+        var cpfLOC = document.getElementById("iptCpf").value;
+        var emailLOC = document.getElementById("iptEmail").value;
+        var numTelLOC = document.getElementById("iptNumTel").value;
+        var instaLOC = document.getElementById("iptInstaPerfil").value;
+        var faceLOC = document.getElementById("iptFacePerfil").value;
+        
+        var decisaoALJ;
+
+        function escolhaTipoALJ(){
+            for (var i = 0; i < escALJ.length; i++) {
+                if (escALJ[i].checked) {
+                    decisaoALJ = escALJ[i].value;
+                break;
+                }
+            } 
+        }
+
+        if (!nomeALJ || !ruaALJ || !cidadeALJ || !bairoALJ || !estadoALJ || !complementoALJ || !cepALJ || !escALJ || !nomeLOC
+            || !cpfLOC || !emailLOC || !numTelLOC || !instaLOC || !faceLOC) {
+            alert("Por favor, preencha todos os campos corretamente.");
+            return false;
+        }
+
+        var house = {
+            nomeAlojamento: nomeALJ,
+            ruaAlojamento: ruaALJ,
+            cidadeAlojamento: cidadeALJ,
+            bairoAlojamento: bairoALJ,
+            estadoAlojamento: estadoALJ,
+            complementoAlojamento: complementoALJ,
+            cepAlojamento: cepALJ,
+            decisaoAlojamento: decisaoALJ,
+            nomeLocador: nomeLOC,
+            cpfLocador: cpfLOC,
+            emailLocador: emailLOC,
+            numTelLocador: numTelLOC,
+            instaLocador: instaLOC,
+            faceLocador: faceLOC
+        };
+
+        var storedHouses = localStorage.getItem("registeredHouses");
+        var houses = storedHouses ? JSON.parse(storedHouses) : [];
+
+        houses.push(house);
+
+        localStorage.setItem("registeredHouses", JSON.stringify(houses));
+
+        alert("Casa registrada corretamente.");
+        alert(JSON.stringify(house));
+        
+        return false;
+    });
+});
---
+++ src/views/js/register_script.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("registrationForm").addEventListener("submit", function(event) {
+      event.preventDefault();
+  
+      var email = document.getElementById("emailInput").value;
+      var username = document.getElementById("usernameInput").value;
+      var password = document.getElementById("passwordInput").value;
+      var confirmPassword = document.getElementById("confirmPasswordInput").value;
+  
+      if (!email || !username || !password || !confirmPassword) {
+        alert("Por favor, preencha todos os campos.");
+        return false;
+      }
+  
+      if (password !== confirmPassword) {
+        alert("As senhas não correspondem. Por favor, tente novamente.");
+        return false;
+      }
+  
+      if (password.length < 6) {
+        alert("A senha deve ter no mínimo 6 caracteres.");
+        return false;
+      }
+  
+      var user = {
+        email: email,
+        username: username,
+        password: password
+      };
+  
+      var storedUsers = localStorage.getItem("registeredUsers");
+      var users = storedUsers ? JSON.parse(storedUsers) : [];
+  
+      var existingUser = users.find(function(u) {
+        return u.username === user.username;
+      });
+  
+      if (existingUser) {
+        alert("Usuário já cadastrado. Por favor, escolha um nome de usuário diferente.");
+        return false;
+      }
+  
+      users.push(user);
+  
+      localStorage.setItem("registeredUsers", JSON.stringify(users));
+  
+      alert("Usuário cadastrado com sucesso!");
+  
+      window.location.href = "login.html";
+  
+      return false;
+    });
+  });
+  
